c85cf51fc650454d780062057f0e507270787ef7
Pedro Medeiros <pedromed.silva05@gmail.com>
2024-04-22 12:42:47 -0300

    Programa Exercicio01

--- Projeto/src/br/edu/up/Programa.java
+++ Projeto/src/br/edu/up/Programa.java
@@ -1,6 +1,12 @@
 package br.edu.up;
+
+import br.edu.up.exercicios.Exercicio01;
+
 public class Programa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+        Exercicio01 exercicio01 = new Exercicio01();
+        exercicio01.Executar();
+
+     }
 }
