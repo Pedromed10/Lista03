0b6a2139e7a786cfc43939206f16764b5efdc856
Pedro Medeiros <pedromed.silva05@gmail.com>
2024-04-22 12:39:49 -0300

    Exercicio01

---
+++ Projeto/src/br/edu/up/exercicios/Exercicio01.java
@@ -0,0 +1,22 @@
+package br.edu.up.exercicios;
+
+import br.edu.up.modelos.*;
+import br.edu.up.*;
+
+public class Exercicio01 {
+    public void Executar() {
+
+        String nome = "João";
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        double nota3 = 6.5;
+
+        Aluno aluno = new Aluno(nome, nota1, nota2, nota3);
+
+        double media = aluno.calcularMedia();
+
+        Prompt.separador();
+        Prompt.imprimir("Nome do aluno: " + aluno.nome);
+        Prompt.imprimir("Média: " + media);
+    }
+}
